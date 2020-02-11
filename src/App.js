a802f9ff22a1d816b9d295cade412c8823bf0616
Hotelsoft User <user1@192.168.1.6>
2020-02-11 22:50:19 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import headerT from "./header";
 
-function App() {
-  return (
-    <div className="App">
-      <headerT />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <headerT />
+      </div>
+    );
+  }
 }
 
 export default App;
