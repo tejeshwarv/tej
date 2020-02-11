6c9ed9a315910caacc7343a6e8dc33ac7c16eb67
Hotelsoft User <user1@192.168.1.6>
2020-02-11 22:41:00 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import header from "./header";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello dinesh</h1>
+      <header />
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function header() {
+  return (
+    <div>
+      <h1>hello tej</h1>
+    </div>
+  );
+}
+
+export default header;
