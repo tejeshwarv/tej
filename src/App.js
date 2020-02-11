64568c4a5a5a66af26fc92d6cb37b2662d1ae1af
Hotelsoft User <user1@192.168.1.6>
2020-02-11 23:38:04 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+export default function One() {
+  return <h1>ok</h1>;
+}
+
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <One />
     </div>
   );
 }
