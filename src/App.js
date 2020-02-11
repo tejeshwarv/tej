201638ec9f06b9f9907e33d578c17ac97ad8b5a3
Hotelsoft User <user1@192.168.1.6>
2020-02-11 23:48:05 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Heading from "./heading";
+
 export function One() {
   return <h1>ok</h1>;
 }
@@ -11,6 +13,7 @@ function App() {
     <div className="App">
       <h1>hello</h1>
       <One />
+      <Heading />
     </div>
   );
 }
---
+++ src/heading.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function heading() {
+  return (
+    <div>
+      <h2>good</h2>
+    </div>
+  );
+}
